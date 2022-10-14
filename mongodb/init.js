b15a4ec7692d467a5a115555fed6fcca319db48f
Zachary <zachness2003@gmail.com>
2022-10-14 19:47:43 +0000

    Can create servers

--- mongodb/init.js
+++ mongodb/init.js
@@ -1,3 +1,4 @@
 db = new Mongo().getDB("DiscordClone");
 
-db.users.insertOne({uuid: 0, username: "root", password: "root"});
+db.users.insertOne({uuid: 0, username: "root", password: "root", servers: [0]});
+db.servers.insertOne({ usid: 0, name: "rootServer", owner: 0, users: [0], messages: [{ umid: 0, user: 0, date: 1665776198931, content: "Hello World!"}] });
