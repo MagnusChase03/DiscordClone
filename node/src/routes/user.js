3cb74d7a18aaa84f07c53edeb32e7d08eeae48f7
Zachary <zachness2003@gmail.com>
2022-10-14 15:59:42 +0000

    Can add users to database

--- node/src/routes/user.js
+++ node/src/routes/user.js
@@ -1,20 +1,31 @@
 const express = require('express');
+const conn = require('../db/conn');
 const db = require('../db/conn');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-
-    username = req.query.username;
-    if (username != "") {
+router.route('/')
+    .get(async (req, res) => {
 
         var conn = db.getDB();
         var users = await conn.collection('users').find({}).toArray();
 
         res.send(users);
 
-    }
+    })
+    .post(async (req, res) => {
 
-});
+        var conn = db.getDB();
+        var lastUser = await conn.collection('users').find({}).sort({_id:-1}).limit(1).toArray();
+
+        conn.collection('users').insertOne({ 
+            uuid: lastUser[0].uuid + 1, 
+            username: req.body.username, 
+            password: req.body.password 
+        });
+
+        res.json({"Stauts": "Ok"});
+
+    });
 
 module.exports = router;
