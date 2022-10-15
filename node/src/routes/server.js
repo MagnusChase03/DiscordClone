4dd8261b4d925a04bdab74f21fe66a5cb28f5b11
Zachary <zachness2003@gmail.com>
2022-10-15 14:55:26 -0500

    Can send and retrive all messages

--- node/src/routes/server.js
+++ node/src/routes/server.js
@@ -36,7 +36,7 @@ router.route('/')
 
             } else {
 
-                req.status(401);
+                res.status(401);
                 res.json({ "Status": "Failed auth" });
 
             }
@@ -70,7 +70,7 @@ router.route('/invite')
 
                 } else {
 
-                    req.status(401);
+                    res.status(401);
                     res.json({ "Status": "Failed auth" });
 
                 }
@@ -78,7 +78,7 @@ router.route('/invite')
 
             } else {
 
-                req.status(401);
+                res.status(401);
                 res.json({ "Status": "Failed auth" });
 
             }
@@ -90,7 +90,7 @@ router.route('/invite')
 
         }
 
-    });
+});
 
 router.route('/join')
     .post(async (req, res) => {
@@ -122,7 +122,129 @@ router.route('/join')
 
             } else {
 
-                req.status(401);
+                res.status(401);
+                res.json({ "Status": "Failed auth" });
+
+            }
+
+        } else {
+
+            res.status(404);
+            res.json({ "Status": "Token does not exit" });
+
+        }
+
+});
+
+router.route('/message')
+    .get(async (req, res) => {
+
+        var token = req.headers.token;
+        if (token != null) {
+
+            var user = userTokens.getTokens().get(token);
+            if (user != null) {
+
+                if (user == parseInt(req.headers.uuid)) {
+
+                    var conn = db.getDB();
+                    var servers = await conn.collection('servers').find({ usid: parseInt(req.headers.usid) }).limit(1).toArray();
+
+                    var isAUser = false;
+                    for (var i = 0; i < servers[0].users.length; i++) {
+
+                        if (servers[0].users[i] == user) {
+
+                            isAUser = true;
+                            break;
+
+                        }
+
+                    }
+
+                    if (isAUser) {
+
+                        res.json({ "Status": "Ok", "messages": servers[0].messages });
+
+                    } else {
+
+                        res.status(401);
+                        res.json({ "Status": "User not in server" });
+
+                    }
+
+                } else {
+
+                    res.status(401);
+                    res.json({"Status": "Failed auth"});
+
+                }
+                
+
+            } else {
+
+                res.status(404);
+                res.json({ "Status": "Token does not exit" });
+
+            }
+
+        } else {
+
+            res.status(400);
+            res.json({ "Status": "No token given" });
+
+        }
+
+    })
+    .post(async (req, res) => {
+
+        var user = userTokens.getTokens().get(req.body.token);
+        if (user != null) {
+
+            if (user == parseInt(req.body.uuid)) {
+
+                var conn = db.getDB();
+
+                var servers = await conn.collection('servers').find({ usid: parseInt(req.body.usid) }).limit(1).toArray();
+
+                var isAUser = false;
+                for (var i = 0; i < servers[0].users.length; i++) {
+
+                    if (servers[0].users[i] == user) {
+
+                        isAUser = true;
+                        break;
+
+                    }
+
+                }
+
+                if (isAUser) {
+
+                    var lastUmid = servers[0].messages[servers[0].messages.length - 1].umid;
+                    var date = new Date().getTime();
+
+                    await conn.collection('servers').updateOne({ usid: parseInt(req.body.usid) }, { $push: { messages: {
+
+                        umid: lastUmid + 1,
+                        date: date,
+                        user: user,
+                        content: req.body.content
+
+                    }}});
+
+                    res.json({"Status": "Ok"});
+
+                } else {
+
+                    res.status(401);
+                    res.json({ "Status": "User not in server" });
+
+                }
+
+            } else {
+
+                res.status(401);
                 res.json({ "Status": "Failed auth" });
 
             }
