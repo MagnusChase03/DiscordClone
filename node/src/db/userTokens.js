bdd408e7a30e45f7e0f44681800c0e9157062d8b
Zachary <zachness2003@gmail.com>
2022-10-15 10:50:37 -0500

    Can invite people to servers, increase token useage

---
+++ node/src/db/userTokens.js
@@ -0,0 +1,21 @@
+var tokens = new Map();
+
+module.exports = {
+
+    getTokens: function () {
+
+        return tokens;
+
+    },
+
+    generateToken: function() {
+        let result = [];
+        let hexRef = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+
+        for (let n = 0; n < 18; n++) {
+            result.push(hexRef[Math.floor(Math.random() * 16)]);
+        }
+        return result.join('');
+    }
+
+};
