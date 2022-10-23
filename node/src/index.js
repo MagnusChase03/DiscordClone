ff8e9ff66ea0c997da7731706f85e20e5ec14e79
Zachary <zachness2003@gmail.com>
2022-10-23 15:26:33 -0500

    Self signed https

--- node/src/index.js
+++ node/src/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db/conn');
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+    key: fs.readFileSync('certs/key.pem'),
+    cert: fs.readFileSync('certs/cert.pem')
+};
 
 const app = express()
 const port = 3000
@@ -14,9 +21,17 @@ app.use('/user', userRoutes);
 const serverRoutes = require('./routes/server');
 app.use('/server', serverRoutes);
 
-app.listen(port, async () => {
+// app.listen(port, async () => {
+
+//     await db.connect();
+//     console.log(`Example app listening on port ${port}`);
+
+// });
+
+var server = https.createServer(options, app)
+server.listen(3000, async() => {
 
     await db.connect();
-    console.log(`Example app listening on port ${port}`);
+    console.log("Listening on 3000");
 
 });
